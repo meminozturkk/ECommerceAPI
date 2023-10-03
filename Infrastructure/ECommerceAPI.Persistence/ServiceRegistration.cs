91871cb52d91cc28bea989c39272e1aeb46fcf45
muham <muham@MEMINOZTURK>
2023-10-04 01:48:16 +0300

    Add project files.

---
+++ Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using ECommerceAPI.Persistence.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ECommerceAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+        }
+    }
+}
