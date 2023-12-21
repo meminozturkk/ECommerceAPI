77106f052f968d09a92ca9399dbea60edbc15899
meminozturk <muham@MEMINOZTURK>
2023-12-21 23:38:14 +0300

    user controller

---
+++ Core/ECommerceAPI.Application/Exceptions/UserCreateFailedException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Exceptions
+{
+    public class UserCreateFailedException : Exception
+    {
+        public UserCreateFailedException() : base("When creating user, a problem was occurred")
+        {
+        }
+
+        public UserCreateFailedException(string? message) : base(message)
+        {
+        }
+
+        public UserCreateFailedException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
